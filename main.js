559b8c62f6da2f46b2bcc638b83f58082249f632
Fernando da Ros <fdaros@fluxo.engenharia.com.br>
2017-08-23 18:54:27 -0300

    Small changes

--- main.js
+++ main.js
@@ -4,10 +4,9 @@ function createProject()
     $.ajax({
       type: "POST",
       url: "http://localhost/tutorial/newProject.php",
-      data: {pName: newProjectName, username: 'fdr'}
+      data: {pName: newProjectName, username: localStorage.getItem('email')}
     }).done(function(msg) {
         document.getElementById("projectModalError").innerHTML = msg;
-      alert(msg);
     }); 
 }
 
@@ -20,8 +19,17 @@ function clearModal()
 
 function loadProjects()
 {       
-    console.log(localStorage.getItem('name'));
-    document.getElementById('myProjects').innerHTML = '';
-    $("#myProjects").append('<li><a href="#1">Project 1</a></li>');
-    $("#myProjects").append('<li><a href="#" class="add-project" data-toggle="modal" data-target="#add_project" onClick="clearModal()">Add Project</a></li>');
+    $.ajax({
+      type: "POST",
+      url: "http://localhost/tutorial/projectsManager.php", //getProjects
+      data: {email: localStorage.getItem('email')}
+    }).done(function(response) {
+        var projects = JSON.parse(response);
+        document.getElementById('myProjects').innerHTML = '';
+        var i;
+        for(i = 0; i < projects.length; i++) {
+            $("#myProjects").append('<li><a href="#1">' + projects[i] + '</a></li>');
+        }
+        $("#myProjects").append('<li><a href="#" class="add-project" data-toggle="modal" data-target="#add_project" onClick="clearModal()">Add Project</a></li>');
+    });
 }
