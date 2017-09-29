cecf7d7927ab38c8c4673a0056f4686fba9ae204
Fernando da Ros <fdaros@fluxo.engenharia.com.br>
2017-09-29 19:02:32 -0300

    Add git ignore

--- calculator/GroundingSystems/main.cpp
+++ calculator/GroundingSystems/main.cpp
@@ -1,6 +1,9 @@
 #include <QCoreApplication>
 #include <QDebug>
+#include <QSettings>
 #include "groundingsystem.h"
+
+void loadCables();
 
 int main(int argc, char *argv[])
 {
@@ -9,6 +12,29 @@ int main(int argc, char *argv[])
     GroundingSystem *gs = new GroundingSystem();
     gs->load();
 
+    loadCables();
     qDebug() << "done";
     return a.exec();
+}
+
+void loadCables()
+{
+    QString filename = "C:\\Users\\fdaros\\Desktop\\chevron - Adaptado.ftl";
+    QSettings settings(filename, QSettings::IniFormat);
+
+//    settings.beginGroup("TransmissionLines");
+//    for(int i = 0; i < nCables; ++i) {
+//        settings->setArrayIndex(i);
+//        CablePtr c = std::make_shared<Cable>(shared_from_this());
+//        c->load(settings);
+//        m_cables.push_back(c);
+//    }
+//    settings.endGroup();
+
+    settings.beginGroup("TransmissionLines");
+    for(const QString& transmissionLineGroup : settings.childGroups()) {
+        qDebug() << "tl";
+        settings.endGroup();
+    }
+    settings.endGroup();
 }
