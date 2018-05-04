4ff10248344cef132f3291c5c3de2dec09259934
Fernando Da Rós Filho <fdarosfilho@gmail.com>
2018-05-04 09:14:25 -0300

    Replace background img

--- application/views/login/register_view.php
+++ application/views/login/register_view.php
@@ -2,13 +2,25 @@
     <head>
         <meta charset="UTF-8">
         <link rel="stylesheet" href="<?php echo base_url(); ?>css/login.css">
+        <style>
+            #body {
+            /*
+                background: url('http://relaypowersystems.com/wp-content/uploads/2011/11/substation.jpg') fixed;
+                            background-size: cover;
+                padding: 0;
+                margin: 0;
+            */
+
+            background-color: grey;
+        }
+        </style>
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
         <title>Malhas de Terra</title>
         <link rel="shortcut icon" type="image/png" href="http://simpleicon.com/wp-content/uploads/lightning.png" />
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>        
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>   
     </head>
-    <body >
+    <body id="body">
         <div class="container">
             <div class="row">
                 <div class="col-md-12">
